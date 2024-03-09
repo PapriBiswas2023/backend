a743338c21b17420d74ff5a79c47410f6fc48228
PapriBiswas2023 <official.p2023@gmail.com>
2024-03-09 16:33:16 +0530

    major changes

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.get('/',(req,res)=>{
+  res.send('df')
+})
+app.listen(PORT,()=>{
+    console.log(`you have in ${PORT}`)
+})
