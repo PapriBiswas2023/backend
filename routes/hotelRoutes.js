5dc3bb87e61c99062cd662cd7b35f6b60bcfc4e0
PapriBiswas2023 <official.p2023@gmail.com>
2024-03-12 20:03:06 +0530

    new-modules

--- routes/hotelRoutes.js
+++ routes/hotelRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const hotelRouter = express.hotelRouter();
 
+router.get('/', hotelController.getItems);
+
 
 module.exports = hotelRouter;
--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const roomRouter = express.roomRouter();
 
-
+router.get('/', roomController.getItems);
 module.exports = roomRouter;
